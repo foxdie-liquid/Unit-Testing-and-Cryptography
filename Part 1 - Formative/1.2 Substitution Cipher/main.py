e21e7b647ed68b583eefe715b4f830a3123dd38d
219137 <zekrom312@gmail.com>
2024-09-05 10:37:16 -0400

    finished formative 1.2

--- Part 1 - Formative/1.2 Substitution Cipher/main.py
+++ Part 1 - Formative/1.2 Substitution Cipher/main.py
@@ -4,11 +4,19 @@ alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 def sub_encode(text, codebet):
-    return ""
+    new_str = ""
+    for let in text:
+        index_alpha = alpha.find(let)
+        new_str += cipher_alphabet[index_alpha]
+    return new_str
 
 
 def sub_decode(text, codebet):
-    return ""
+    new_str = ""
+    for let in text:
+        index_cipher = cipher_alphabet.find(let)
+        new_str += alpha[index_cipher]
+    return new_str
 
 
 test = "HELLOWORLD"
