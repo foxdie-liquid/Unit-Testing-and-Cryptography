d7a975d5c049ef9daa36032e7ce55b138c4f92a2
219137 <zekrom312@gmail.com>
2024-09-05 11:11:02 -0400

    working on formative 1.3.2 (unit testing substitution)

--- Part 1 - Formative/1.2 Substitution Cipher/main.py
+++ Part 1 - Formative/1.2 Substitution Cipher/main.py
@@ -2,19 +2,18 @@
 
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-
 def sub_encode(text, codebet):
     new_str = ""
     for let in text:
         index_alpha = alpha.find(let)
-        new_str += cipher_alphabet[index_alpha]
+        new_str += codebet[index_alpha]
     return new_str
 
 
 def sub_decode(text, codebet):
     new_str = ""
     for let in text:
-        index_cipher = cipher_alphabet.find(let)
+        index_cipher = codebet.find(let)
         new_str += alpha[index_cipher]
     return new_str
 
---
+++ Part 1 - Formative/1.2 Substitution Cipher/tests/test_sub_encode.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from main import sub_encode
+
+class TestInsertString(TestCase):
+    def test_insert_string_two_words_lowercase(self):
+        self.assertEqual(sub_encode("HELLOWORLD", codebet), "MXTTHAHOTU")
