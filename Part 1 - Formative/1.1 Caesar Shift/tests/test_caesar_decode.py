2a064df55c29ea75d8555a7282af20c566b22c0f
219137 <zekrom312@gmail.com>
2024-09-09 10:56:27 -0400

    added more to caesar shift tests

--- Part 1 - Formative/1.1 Caesar Shift/tests/test_caesar_decode.py
+++ Part 1 - Formative/1.1 Caesar Shift/tests/test_caesar_decode.py
@@ -6,5 +6,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from main import caesar_decode
 
 class TestInsertString(TestCase):
-    def test_insert_string_two_words_lowercase(self):
+    def test_shift_normal(self):
         self.assertEqual(caesar_decode("MJQQTBTWQI", 5), "HELLOWORLD")
+    def test_shift_7(self):
+        self.assertEqual(caesar_decode("MJQQTBTWQI", 7), "FCJJMUMPJB")
+    def test_shift_21(self):
+        self.assertEqual(caesar_decode("MJQQTBTWQI", 21), "ROVVYGYBVN")
+    def test_shift_255minus10(self):
+        self.assertEqual(caesar_decode("MJQQTBTWQI", 255-10), "BYFFIQILFX")
+    def test_shift_1000minus25(self):
+        self.assertEqual(caesar_decode("MJQQTBTWQI", 1000-25), "ZWDDGOGJDV")
--- Part 1 - Formative/1.1 Caesar Shift/tests/test_caesar_encode.py
+++ Part 1 - Formative/1.1 Caesar Shift/tests/test_caesar_encode.py
@@ -8,3 +8,11 @@ from main import caesar_encode
 class TestInsertString(TestCase):
     def test_shift_normal(self):
         self.assertEqual(caesar_encode("HELLOWORLD", 5), "MJQQTBTWQI")
+    def test_shift_7(self):
+        self.assertEqual(caesar_encode("HELLOWORLD", 7), "OLSSVDVYSK")
+    def test_shift_21(self):
+        self.assertEqual(caesar_encode("HELLOWORLD", 21), "CZGGJRJMGY")
+    def test_shift_255(self):
+        self.assertEqual(caesar_encode("HELLOWORLD", 255), "CZGGJRJMGY")
+    def test_shift_1000(self):
+        self.assertEqual(caesar_encode("HELLOWORLD", 1000), "TQXXAIADXP")
