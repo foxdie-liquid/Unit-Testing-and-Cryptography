3f6b37a2d51b014a132b87c769c66a9ab2ec0348
219137 <zekrom312@gmail.com>
2024-09-18 10:34:23 -0400

    completed vigenere cipher

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -1,13 +1,25 @@
 # Read the instructions to see what you need to do here!
 
-alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_"
 
 def vig_encode(text, keyword):
-  return ""
+  new_str = ""
+  for i in range(len(text)):
+    ind = alpha.find(text[i]) + alpha.find(keyword[i % len(keyword)])
+    ind = ind % 27
+    new_str += alpha[ind]
+  return new_str
+
+
 
 
 def vig_decode(text, keyword):
-  return ""
+  new_str = ""
+  for i in range(len(text)):
+    ind = alpha.find(text[i]) - alpha.find(keyword[i % len(keyword)])
+    ind = ind % 27
+    new_str += alpha[ind]
+  return new_str
 
 
 test = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
