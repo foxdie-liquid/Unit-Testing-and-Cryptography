b842f7dc67051656a89772b55663f1480e97cc0e
219137 <zekrom312@gmail.com>
2024-09-23 10:39:36 -0400

    finished unit testing affine_encode & affine_decode

--- Part 2 - Summative/2.2 Affine Cipher/test/test_affine_decode.py
+++ Part 2 - Summative/2.2 Affine Cipher/test/test_affine_decode.py
@@ -8,3 +8,11 @@ from main import affine_decode
 class TestInsertString(TestCase):
     def test_affine_decode_normal(self):
         self.assertEqual(affine_decode("EVQQZXZIQS", 3, 9), "HELLOWORLD")
+    def test_affine_decode_1n5(self):
+        self.assertEqual(affine_decode("HELLOWORLD", 1, 5), "ITCCRFRGCO")
+    def test_affine_decode_3n7(self):
+        self.assertEqual(affine_decode("CZGGJRJMGY", 3, 7), "LQNNIMIDNJ")
+    def test_affine_decode_7n6(self):
+        self.assertEqual(affine_decode("HGRRSMSTRX", 7, 6), "JDRRXNXDRB")
+    def test_affine_decode_5n6(self):
+        self.assertEqual(affine_decode("PAJJYMYNJV", 5, 6), "HVXXJPJVXR")
